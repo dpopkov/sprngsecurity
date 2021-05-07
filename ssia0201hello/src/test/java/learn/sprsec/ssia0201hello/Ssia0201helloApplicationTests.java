b68525c2cb22f4a15213c57766cce55d6efe4665
Denis Popkov <pkvdenis@gmail.com>
2021-05-07 21:14:53 +0300

    ssia0201hello: add test.

--- ssia0201hello/src/test/java/learn/sprsec/ssia0201hello/Ssia0201helloApplicationTests.java
+++ ssia0201hello/src/test/java/learn/sprsec/ssia0201hello/Ssia0201helloApplicationTests.java
@@ -1,13 +1,47 @@
 package learn.sprsec.ssia0201hello;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Ssia0201helloApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void contextLoads() {
+    @DisplayName("Test calling /hello endpoint without authentication returns unauthorized.")
+    void helloUnauthenticated() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isUnauthorized());
     }
 
+    @Test
+    @DisplayName("Test calling /hello endpoint authenticated with a mock user returns ok.")
+    @WithMockUser
+    void helloAuthenticated() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(content().string("Hello!"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test calling /hello endpoint authenticated with a real user returns ok.")
+    void helloAuthenticatedWithUser() throws Exception {
+        mvc.perform(get("/hello")
+                .with(user("mary"))
+        )
+                .andExpect(content().string("Hello!"))
+                .andExpect(status().isOk());
+    }
 }
